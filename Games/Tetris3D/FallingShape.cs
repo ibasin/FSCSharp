f4d2d965ef918eb9aae766a801b733125f3d869b
Ilya Basin (LG) <ilya.basin@gmail.com>
2026-05-22 11:51:51 -0400

    progress

--- Games/Tetris3D/FallingShape.cs
+++ Games/Tetris3D/FallingShape.cs
@@ -105,6 +105,7 @@ public class FallingShape : TangibleGameObject
                 {
                     var xx = (int)Math.Round(Location.X + x * Tetris3DGame.SquareSide);
                     var yy = (int)Math.Round(Location.Y + y * Tetris3DGame.SquareSide);
+                    
                     //Raylib.DrawRectangle(xx, yy, Tetris3DGame.SquareSide, Tetris3DGame.SquareSide, color.Value);
                 }
             }
