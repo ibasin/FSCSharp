107a65a039bdc14b471744b83172973c9242d803
Ilya Basin (LG) <ilya.basin@gmail.com>
2026-06-01 21:07:42 -0400

    progress

---
+++ Games/RunningBunny/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RunningBunny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
