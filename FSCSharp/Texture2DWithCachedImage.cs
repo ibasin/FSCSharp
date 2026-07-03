5f302b42c70ea997f0f70a15be628d98b68d4503
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-07-03 12:55:00 -0400

    progress

---
+++ FSCSharp/Texture2DWithCachedImage.cs
@@ -0,0 +1,34 @@
+﻿using Raylib_cs;
+
+namespace FSCSharp;
+
+public class Texture2DAndCachedImage : IDisposable
+{
+    #region Constructors
+    public Texture2DAndCachedImage(Texture2D texture)
+    {
+        Texture = texture;
+    }
+    #endregion
+
+    public void Dispose()
+    {
+        Raylib.UnloadTexture(Texture);
+        if (_image.HasValue) Raylib.UnloadImage(_image.Value);
+    }
+
+    #region Properties
+    //this is intentionally not a property b/c this is a struct and we want to avoid copying it around
+    public Texture2D Texture; 
+
+    public Image Image 
+    {
+        get
+        {
+            if (!_image.HasValue) _image = Raylib.LoadImageFromTexture(Texture);
+            return _image.Value;
+        }
+    }
+    private Image? _image;
+    #endregion
+}
