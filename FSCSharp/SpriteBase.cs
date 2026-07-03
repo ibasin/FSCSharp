cebd9c0d483936dd0b17cd772e775a86bee16be7
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-07-03 02:05:18 -0400

    progress

--- FSCSharp/SpriteBase.cs
+++ FSCSharp/SpriteBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using Microsoft.VisualBasic;
 using Raylib_cs;
 
 namespace FSCSharp;
@@ -60,30 +61,18 @@ public abstract class SpriteBase : IDisposable
         var otherY1 = otherSpriteLocation.Y + otherHalfSize.Y;
 
         var xIntersect = false;
-        if (myX0 >= otherX0 && myX0 <= otherX1) xIntersect = true;
-        if (otherX0 >= myX0 && otherX0 <= myX1) xIntersect = true;
-        if (myX1 >= otherX0 && myX1 <= otherX1) xIntersect = true;
-        if (otherX1 >= myX0 && otherX1 <= myX1) xIntersect = true;
+        if (otherX0 <= myX0 && myX0 <= otherX1) xIntersect = true;
+        if (otherX0 <= myX1 && myX1 <= otherX1) xIntersect = true;
+        if (myX0 <= otherX0 && otherX0 <= myX1) xIntersect = true;
+        if (myX0 <= otherX1 && otherX1 <= myX1) xIntersect = true;
 
         var yIntersect = false;
-        if (myY0 >= otherY0 && myY0 <= otherY1) yIntersect = true;
-        if (otherY0 >= myY0 && otherY0 <= myY1) yIntersect = true;
-        if (myY1 >= otherY0 && myY1 <= otherY1) yIntersect = true;
-        if (otherY1 >= myY0 && otherY1 <= myY1) yIntersect = true;
+        if (otherY0 <= myY0 && myY0 <= otherY1) yIntersect = true;
+        if (otherY0 <= myY1 && myY1 <= otherY1) yIntersect = true;
+        if (myY0 <= otherY0 && otherY0 <= myY1) yIntersect = true;
+        if (myY0 <= otherY1 && otherY1 <= myY1) yIntersect = true;
 
         return xIntersect && yIntersect;
-
-        // ReSharper disable once ReplaceWithSingleAssignment.True
-        //var result = true;
-
-
-        //if (myLocation.X - myHalfSize.X > otherSpriteLocation.X + otherHalfSize.X) result = false;
-        //if (otherSpriteLocation.X - otherHalfSize.X > myLocation.X + myHalfSize.X) result = false;
-
-        //if (myLocation.Y - myHalfSize.Y > otherSpriteLocation.Y + otherHalfSize.Y) result = false;
-        //if (otherSpriteLocation.Y - otherHalfSize.Y > myLocation.Y + myHalfSize.Y) result = false;
-
-        //return result;
     }
     public virtual void SetScalesByTargetRectangleSize(float x, float y)
     {
