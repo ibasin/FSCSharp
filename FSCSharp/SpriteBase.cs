cdeb333ba0f66b0c952336764e592f3cf0b0d746
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-07-03 01:34:09 -0400

    progress

--- FSCSharp/SpriteBase.cs
+++ FSCSharp/SpriteBase.cs
@@ -46,19 +46,44 @@ public abstract class SpriteBase : IDisposable
     }
     public virtual bool IsCollidingAtLocation(Vector2 myLocation, SpriteBase otherSprite, Vector2 otherSpriteLocation)
     {
-        // ReSharper disable once ReplaceWithSingleAssignment.True
-        var result = true;
-
         var myHalfSize = Size / 2;
         var otherHalfSize = otherSprite.Size / 2;
 
-        if (myLocation.X - myHalfSize.X > otherSpriteLocation.X + otherHalfSize.X) result = false;
-        if (otherSpriteLocation.X - otherHalfSize.X > myLocation.X + myHalfSize.X) result = false;
+        var myX0 = myLocation.X - myHalfSize.X;
+        var myX1 = myLocation.X + myHalfSize.X;
+        var myY0 = myLocation.Y - myHalfSize.Y;
+        var myY1 = myLocation.Y + myHalfSize.Y;
 
-        if (myLocation.Y - myHalfSize.Y > otherSpriteLocation.Y + otherHalfSize.Y) result = false;
-        if (otherSpriteLocation.Y - otherHalfSize.Y > myLocation.Y + myHalfSize.Y) result = false;
+        var otherX0 = otherSpriteLocation.X - otherHalfSize.X;
+        var otherX1 = otherSpriteLocation.X + otherHalfSize.X;
+        var otherY0 = otherSpriteLocation.Y - otherHalfSize.Y;
+        var otherY1 = otherSpriteLocation.Y + otherHalfSize.Y;
 
-        return result;
+        var xIntersect = false;
+        if (myX0 >= otherX0 && myX0 <= otherX1) xIntersect = true;
+        if (otherX0 >= myX0 && otherX0 <= myX1) xIntersect = true;
+        if (myX1 >= otherX0 && myX1 <= otherX1) xIntersect = true;
+        if (otherX1 >= myX0 && otherX1 <= myX1) xIntersect = true;
+
+        var yIntersect = false;
+        if (myY0 >= otherY0 && myY0 <= otherY1) yIntersect = true;
+        if (otherY0 >= myY0 && otherY0 <= myY1) yIntersect = true;
+        if (myY1 >= otherY0 && myY1 <= otherY1) yIntersect = true;
+        if (otherY1 >= myY0 && otherY1 <= myY1) yIntersect = true;
+
+        return xIntersect && yIntersect;
+
+        // ReSharper disable once ReplaceWithSingleAssignment.True
+        //var result = true;
+
+
+        //if (myLocation.X - myHalfSize.X > otherSpriteLocation.X + otherHalfSize.X) result = false;
+        //if (otherSpriteLocation.X - otherHalfSize.X > myLocation.X + myHalfSize.X) result = false;
+
+        //if (myLocation.Y - myHalfSize.Y > otherSpriteLocation.Y + otherHalfSize.Y) result = false;
+        //if (otherSpriteLocation.Y - otherHalfSize.Y > myLocation.Y + myHalfSize.Y) result = false;
+
+        //return result;
     }
     public virtual void SetScalesByTargetRectangleSize(float x, float y)
     {
--- Games/WizardOfTerror/StaticObjects/Field.cs
+++ Games/WizardOfTerror/StaticObjects/Field.cs
@@ -28,7 +28,7 @@ public class Field : TangibleGameObject
             for (var y = 0; y < Board.GetLength(1); y++)
             {
                 //Board[x, y] = Random.Shared.Next(64);
-                Board[x, y] = 0;
+                Board[x, y] = 0; //37;
             }
         }
     }
