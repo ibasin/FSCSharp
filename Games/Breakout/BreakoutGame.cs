d45c8ab853af8c8915af8021d83473c9fb4f6dc0
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-09-12 22:13:42 -0400

    progress

---
+++ Games/Breakout/BreakoutGame.cs
@@ -0,0 +1,14 @@
+﻿using FSCSharp;
+using Raylib_cs;
+
+namespace Breakout
+{
+    public class BreakoutGame : Game<BreakoutGame>
+    {
+        public BreakoutGame() : base("Breakout", 540*3, 315*3, Color.Black)
+        {
+            //Current.PlaySound("Resources/kalinka.mp3");
+            Current.ShowSplashScreen("Resources/splash.png", 7750);
+        }
+    }
+}
---
+++ Games/Breakout/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Breakout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (new BreakoutGame().Run()) { }
+        }
+    }
+}
